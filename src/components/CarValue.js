ddbe335fec0484c8c4fd428676ba964b01d187b2
John Vundi <johnsonvundy2@gmail.com>
2023-06-20 16:48:19 +0300

    updated CarValue.js

--- src/components/CarValue.js
+++ src/components/CarValue.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 function CarValue() {
+    const totalCost = useSelector(({cars:{data, searchTerm}})=>{
+        const filteredCars = data.filter((car)=>{
+            return car.name.toLowerCase().includes(searchTerm.toLowerCase())
+        })
+    })
+
   return (
-    <div>CarValue</div>
+    <div className='car-value'>
+     Total Cost: ${totalCost}
+    </div>
   )
 }
 
