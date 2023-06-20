70939ef80c63768f09ddf05df3b9b12bcb8af476
John Vundi <johnsonvundy2@gmail.com>
2023-06-20 12:25:28 +0300

    created cars components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
+// import the React and ReactDom libraries
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import {createRoot} from 'react-dom/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+// Get a reference to the div with ID root
+const el = document.getElementById('root');
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Tell React to take control of that element
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const root = createRoot(el);
+
+
+root.render( <App />);
